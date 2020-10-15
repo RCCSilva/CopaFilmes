12236869b6df6f71d5c7e916ccd96a40d4cdf4a9
rafae.silva <rafael.silva@rappi.com>
2020-10-14 22:21:09 -0300

    fix result text

--- frontend/src/components/result/result-card/ResultCard.style.js
+++ frontend/src/components/result/result-card/ResultCard.style.js
@@ -1,6 +1,6 @@
 export const boxStyle = 'flex w-full w-32 mt-4';
 
 export const placeHolderStyle = 'px-4 py-4 bg-gray-700 text-center font-bold text-white text-4xl';
-export const titleHolderStyle = 'w-full pl-16 bg-white text-xl';
+export const titleHolderStyle = 'w-full pl-16 bg-white flex items-center';
 
-export const titleTextStyle = 'my-auto';
+export const titleTextStyle = 'text-xl';
